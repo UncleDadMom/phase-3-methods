ac3cc44bb639973878d924b4b9f8d1b214d71b22
Patrick Wu <Ptrckwu@yahoo.com>
2021-10-13 11:42:26 -0400

    commit tree 45d994cf4150d2262598f961c110a413d1717f61

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+def add(x, y)
+    p x+y
+end
+def halve(x)
+    if x.class == Integer
+        x/2
+    else 
+        return nil
+    end
+end
